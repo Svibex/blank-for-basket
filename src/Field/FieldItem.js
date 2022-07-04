2aa08fa0c548cea40f5a157d0fa1b780afd02221
Svibex <future.science@yandex.ru>
2022-07-04 09:50:37 +0300

    feat: add PropTypes

--- src/Field/FieldItem.js
+++ src/Field/FieldItem.js
@@ -1,9 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function FieldItem({ field }) {
+function FieldItem({ field, id }) {
     return (
-        <li>{field.index + field.name + field.price}</li>
+        <li>
+            <strong>{id+1}</strong>
+            {field.index + field.name + field.price}
+        </li>
     )
+}
+
+FieldItem.propTypes = {
+    field: PropTypes.object.isRequired,
+    id: PropTypes.number
 }
 
 export default FieldItem;
--- src/Field/FieldList.js
+++ src/Field/FieldList.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import FieldItem from "./FieldItem";
+import PropTypes from "prop-types";
 
 function FieldList(props) {
     return(
         <ul>
-            {props.fields.map(field => {
-                return <FieldItem field={field} key={field.id} />
+            {props.fields.map((field, id) => {
+                return <FieldItem field={field} key={field.id} id={id}/>
             })}
         </ul>
     )
+}
+
+FieldList.propTypes = {
+    fields: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
 export default FieldList;
