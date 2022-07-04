026d0ba8b11052b5d512f33f16604998e465b0ca
Svibex <future.science@yandex.ru>
2022-07-04 11:30:06 +0300

    feat: add amount items

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import Contex from "./context";
 
 function App() {
   const [fields, setFields] = React.useState([
-    {id:1, index:110, name:'Телефон', price:'10000'},
-    {id:2, index:111, name:'Чайник', price:'2000'},
-    {id:3, index:112, name:'Холодильник', price:'20000'},
-    {id:4, index:113, name:'Микроволновка', price:'5000'},
+    {id:1, index:110, name:'Телефон', price:10000},
+    {id:2, index:111, name:'Чайник', price:2000},
+    {id:3, index:112, name:'Холодильник', price:20000},
+    {id:4, index:113, name:'Микроволновка', price:5000},
   ]);
 
   function deleteField(id) {
       setFields(fields.filter(field => field.id !== id)
   )}
+  
+  const amount = fields.reduce((sum, current) => sum + (current.price || 0), 0);
 
   return (
-      <Contex.Provider value={{deleteField}}>
+      <Contex.Provider value={{ deleteField }}>
         <body>
             <h1>Корзина</h1>
             <span>
                 <h2>Количество товаров в корзине: </h2>
-                <h2>Стоимость товаров в корзине: </h2>
+                <h2>Стоимость товаров в корзине: {amount}</h2>
             </span>
             <FieldList fields={fields}/>
         </body>
